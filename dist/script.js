544156ec758ccdd5a9a6a045f8140fe1013ac07b
Fahad <fahad.45854@iqra.edu.com>
2024-09-07 17:24:34 +0500

    first dumb screen is ready

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,9 @@
 "use strict";
-console.log('Hey there!!!');
+const screenTitle = document.getElementById('title');
+const screenInput = document.getElementById('input');
+const screenNext = document.getElementById('next');
+const infoArray = [];
+infoArray.push(screenInput);
+screenNext === null || screenNext === void 0 ? void 0 : screenNext.addEventListener('click', () => {
+    console.log('clicked', infoArray.length !== 0 ? infoArray : 'Nothing is there in infoArray');
+});
