14dad313932a6c966f91f661cfab238733e7c525
Fahad <fahad.45843@iqra.edu.pk>
2024-09-08 00:24:24 +0500

    title changes as pressed next button

--- dist/script.js
+++ dist/script.js
@@ -1,9 +1,20 @@
 "use strict";
-const screenTitle = document.getElementById('title');
+let screenTitle = document.getElementById('title');
 const screenInput = document.getElementById('input');
 const screenNext = document.getElementById('next');
 const infoArray = [];
-infoArray.push(screenInput);
 screenNext === null || screenNext === void 0 ? void 0 : screenNext.addEventListener('click', () => {
+    screenInput !== null && infoArray.push(screenInput.value);
+    if (screenInput) {
+        screenInput.value = '';
+    }
     console.log('clicked', infoArray.length !== 0 ? infoArray : 'Nothing is there in infoArray');
+    if (screenTitle) {
+        console.log(">..>");
+        screenTitle.innerText = "Select Your Age";
+        if (screenInput) {
+            screenInput.placeholder = "Enter your age";
+        }
+        console.log(">..>end");
+    }
 });
--- script.ts
+++ script.ts
@@ -1,11 +1,23 @@
-const screenTitle: HTMLElement | null = document.getElementById('title');
+let screenTitle: HTMLElement | null = document.getElementById('title');
 const screenInput = document.getElementById('input') as HTMLInputElement | null;
 const screenNext: HTMLElement | null = document.getElementById('next');
 
 const infoArray: any = [];
 
-screenInput!==null && infoArray.push( screenInput.value);
 
 screenNext?.addEventListener('click', () =>{
+    screenInput !== null && infoArray.push(screenInput.value);
+    if(screenInput){
+        screenInput.value = '';
+    }
+
     console.log('clicked', infoArray.length!==0 ? infoArray: 'Nothing is there in infoArray')
+    if(screenTitle){
+        console.log(">..>")
+        screenTitle.innerText = "Select Your Age";
+        if(screenInput){
+            screenInput.placeholder = "Enter your age";
+        }
+        console.log(">..>end")
+    }
 })
