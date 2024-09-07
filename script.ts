5a5d6fbbc564d8c8acc4ea8ee66879543c64f5e3
Fahad <fahad.45854@iqra.edu.com>
2024-09-07 17:26:36 +0500

    cleared a bit

--- script.ts
+++ script.ts
@@ -1,10 +1,10 @@
 const screenTitle: HTMLElement | null = document.getElementById('title');
-const screenInput = document.getElementById('input');
+const screenInput = document.getElementById('input') as HTMLInputElement | null;
 const screenNext: HTMLElement | null = document.getElementById('next');
 
 const infoArray: any = [];
 
-infoArray.push(screenInput!==null && screenInput.value);
+screenInput!==null && infoArray.push( screenInput.value);
 
 screenNext?.addEventListener('click', () =>{
     console.log('clicked', infoArray.length!==0 ? infoArray: 'Nothing is there in infoArray')
